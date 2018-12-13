68a4332d34f4ab2729a68e52d28f008c37d22c9d
Foster McLane <fkmclane@gmail.com>
2018-12-13 09:36:45 -0500

    add mediaelement.js

--- www/carillon.js
+++ www/carillon.js
@@ -3,12 +3,11 @@ var metadata = null;
 var title = null;
 var composer = null;
 var vol = null;
+var volwrap = null;
 var voldown = null;
 var volup = null;
 var play = null;
 var stream = null;
-
-var volume = 0.5;
 
 var xhr = function(method, resource, data, callback) {
 	var req = new XMLHttpRequest();
@@ -45,55 +44,71 @@ var update = function() {
 	setTimeout(update, 1000);
 };
 
-var volupdate = function() {
-	stream.volume = volume;
-	vol.innerText = stream.volume.toFixed(1);
-};
-
 var load = function() {
 	playing = document.getElementById('playing');
 	metadata = document.getElementById('metadata');
 	title = document.getElementById('title');
 	composer = document.getElementById('composer');
 	vol = document.getElementById('vol');
+	volwrap = document.getElementById('volwrap');
 	voldown = document.getElementById('voldown');
 	volup = document.getElementById('volup');
 	play = document.getElementById('play');
 	stream = document.getElementById('stream');
 
-	stream.volume = 0.5;
+	var player = new MediaElementPlayer(document.getElementById('stream'), {
+		features: [],
+		success: function(media, node, player) {
+			stream = media;
 
-	voldown.onclick = function() {
-		volume -= 0.1;
-		if (volume <= 0)
-			volume = 0;
+			var volume = 0.5;
 
-		volupdate();
-	};
+			var volupdate = function() {
+				stream.volume = volume;
+				vol.innerText = stream.volume.toFixed(1);
+			};
 
-	volup.onclick = function() {
-		volume += 0.1;
-		if (volume >= 1)
-			volume = 1;
+			voldown.addEventListener('click', function() {
+				volume -= 0.1;
+				if (volume <= 0)
+					volume = 0;
 
-		volupdate();
-	};
+				volupdate();
+			}, false);
 
-	play.onclick = function() {
-		if (stream.paused) {
-			stream.play();
-			play.innerText = 'Pause';
+			volup.addEventListener('click', function() {
+				volume += 0.1;
+				if (volume >= 1)
+					volume = 1;
+
+				volupdate();
+			}, false);
+
+			var playupdate = function() {
+				if (stream.paused)
+					play.innerText = 'Play';
+				else
+					play.innerText = 'Pause';
+			};
+
+			play.addEventListener('click', function() {
+				if (stream.paused)
+					stream.play();
+				else
+					stream.pause();
+
+				playupdate();
+			}, false);
+
+			volwrap.style.display = 'initial';
+			play.style.display = 'initial';
+			metadata.style.display = 'initial';
+
+			volupdate();
+			playupdate();
+			update();
 		}
-		else {
-			stream.pause();
-			play.innerText = 'Play';
-		}
-	};
-
-	metadata.style.display = 'initial';
-
-	volupdate();
-	update();
+	});
 };
 
 window.addEventListener('load', load, false);
